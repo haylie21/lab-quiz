ab71d623ed2ab4697da8d6ee2cb1e2e224f80682
haylie21 <haylie.antolin08@gmail.com>
2025-03-08 18:28:38 +0800

    Update LQ2_1_ANTOLIN.js

--- LQ2_ANTOLIN_CS1A/LQ2_1_ANTOLIN.js
+++ LQ2_ANTOLIN_CS1A/LQ2_1_ANTOLIN.js
@@ -1,1 +1,8 @@
+let user_name = prompt("Please enter your username:");
+let user_password = prompt("Please enter your password:");
 
+if (user_name === "haylie" && user_password === "haylie21") {
+    alert('Welcome ${user_name}');
+ }  else {
+    alert('Maybe Username or Password is Invalid or Does not Match')
+ }
