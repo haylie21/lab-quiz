964d6cb3d9b4b67b24b11bee0e673969c6431fe4
haylie21 <haylie.antolin08@gmail.com>
2025-03-08 18:31:34 +0800

    Create LQ2_2_ANTOLIN.js

---
+++ LQ2_ANTOLIN_CS1A/LQ2_2_ANTOLIN.js
@@ -0,0 +1,6 @@
+let dog_breed = prompt("Please enter the dog breed:");
+let print_count = parseInt(prompt("Please enter how many count to print:"), 20);
+
+for (let i = 0; i < print_count; i++) {
+    console.log(dog_breed);
+}
